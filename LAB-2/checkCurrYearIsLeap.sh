734b8d6c8fe27d2f9bd0eacaa67a6f2ff7bcb53b
NIRANJAN NAGARAJ SAVANUR <67832520+niru-savanur@users.noreply.github.com>
2021-10-28 12:27:21 +0530

    Add files via upload

---
+++ LAB-2/checkCurrYearIsLeap.sh
@@ -0,0 +1,15 @@
+year=`date +'%Y'`
+
+echo "$year is"
+if [ `expr $year % 400` -eq 0 ]
+then
+    echo "A leap year"
+elif [ `expr $year % 100` -eq 0 ]
+then
+    echo "Not a leap year"
+elif [ `expr $year % 4` -eq 0 ]
+then 
+    echo "A leap year"
+else
+    echo "Not a leap year"
+fi
